4a850d2ca1a16682af32d83d273dd71877da5966
fatigba72 <fatigba72@gmail.com>
2019-07-11 13:28:09 +0000

    feature(Gestion stocks): Ajout de migrations.
    Migration pour la table Commande et ArticleCommande.

---
+++ database/migrations/2019_07_07_142933_create_commandes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommandesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Commande', function (Blueprint $table) {
+            $table->bigIncrements('id_commande');
+            $table->string('emplacement_stock')->nullable();
+            $table->timestamp('date_commande');
+            $table->bigInteger('id_fournisseur');
+            $table->boolean('supprime')->default(false);
+            $table->timestamps();
+
+            $table->foreign("id_fournisseur")->on("Fournisseur")
+                ->references("id_fournisseur") ;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Commande');
+    }
+}
