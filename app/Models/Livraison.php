a1b7e67ae7cd9d775a697c8effeec8f9b8a5870f
fatigba72 <fatigba72@gmail.com>
2019-07-12 20:19:54 +0000

    feature(Gestion stocks): Migration pour la table ArticleLivraison et Livraison.

---
+++ app/Models/Livraison.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Livraison extends Model
+{
+    protected $table ='Livraison' ;
+    protected $primaryKey ='id_livraison' ;
+}
